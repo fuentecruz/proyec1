77724a5f41bc96f0cc35903b47ed89667b22cdc1
fuentecruz <fuentecruz.roy08@gmail.com>
2022-06-09 20:00:38 -0600

    segundo commit interfaz de usuario

---
+++ Oriole.cs
@@ -0,0 +1,8 @@
+// declarar variables
+console.writeline("Bienvenido");
+string usuario;
+int contraseña;
+console.writeline("usuario");
+usuario=string.parse(console.readline());
+console.writeline("contraseña");
+contraseña=int.parse(console.readline());
